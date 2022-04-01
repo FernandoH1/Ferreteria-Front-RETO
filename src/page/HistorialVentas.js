5a99e97ad54779066660a48b1bc9a0b8aff920c5
FernandoH1 <fernandohernandezclivio@gmail.com>
2022-04-01 11:57:17 -0300

    Creacion de la vista de HistorialVentas(page)

---
+++ src/page/HistorialVentas.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Factura from '../components/Factura';
+const HOST_API = "http://localhost:8080";
+const HistorialVentas = () => {
+    const [facturas,setFacturas] = useState([]);
+
+    useEffect(()=>{
+        cargarFacturas();
+
+    },[]);
+
+    const cargarFacturas = ()=>{
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        };
+
+        fetch(HOST_API + "/factura", requestOptions)
+            .then(response => response.json())
+            .then((p) => {
+              
+                setFacturas(p);
+            });
+    }
+    return ( 
+        <div> Ventas
+            {facturas.map( factura =>(
+                 <Factura key={factura.consecutivo_de_facturas}
+                    facture={factura}
+
+                 /> 
+            ))}
+          
+        </div>
+    );
+}
+ 
+export default HistorialVentas;
