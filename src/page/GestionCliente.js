c0418318d545b60a31b0825bb97742b3dcd7e543
FernandoH1 <fernandohernandezclivio@gmail.com>
2022-04-01 11:35:32 -0300

    Creacion de la vista de GestiónCliente(page)

---
+++ src/page/GestionCliente.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useState } from 'react';
+const HOST_API = "http://localhost:8080";
+
+const GestionCliente = () => {
+
+    const [nombre, setNombre] = useState();
+    const [celular, setCelular] = useState();
+    const [ci, setCI] = useState();
+    
+
+    const registrarCliente = (e) => { //registra los clientes cuando hace click en el boton y limpia los campos
+        e.preventDefault();
+
+            if(validarDatos){   
+            let request = {
+                "nombre":nombre,
+                "celular":celular,
+                "documentoID":ci
+            }
+            
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(request)
+            };
+
+            fetch(HOST_API + "/cliente", requestOptions)
+            .then(response => response.json())
+            .then((cliente) => {
+                console.log(cliente);
+            });
+
+            }
+            e.target.reset();
+        }
+    
+
+    const validarDatos = () => {
+        if(nombre!= undefined && celular!= undefined && ci!= undefined){
+            return true;
+        }
+        return false;
+    }
+
+
+    return ( <>
+    <h1>Gestion Cliente</h1>
+    <form onSubmit={registrarCliente}>
+        <label>Nombre</label>
+        <input onChange={ event => {setNombre(event.target.value)}}></input>
+
+        <label>Celular</label>
+        <input onChange={ event => {setCelular(event.target.value)}}></input>
+           
+        <label>C.I.</label>
+        <input onChange={ event => {setCI(event.target.value)}}></input>
+        <button className='btn btn-primary' type='submit'>Crear Cliente</button>
+    </form>
+    </> )
+}
+ 
+export default GestionCliente;
