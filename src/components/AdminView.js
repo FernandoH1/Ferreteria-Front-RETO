341e6b8ccc73a7441ee212648eed543db3fb14f4
FernandoH1 <fernandohernandezclivio@gmail.com>
2022-04-01 12:01:09 -0300

    Se modifico AdminView(components)

--- src/components/AdminView.js
+++ src/components/AdminView.js
@@ -1,7 +1,56 @@
 import React from "react";
+import {BrowserRouter,Routes,Route,Link} from "react-router-dom";
+import GestionCliente from "../page/GestionCliente";
+import GestionProducto from "../page/GestionProducto";
+import GestionProveedor from "../page/GestionProveedor";
+import GestionStock from "../page/GestionStock";
+import GestionVenta from "../page/GestionVenta";
+import HistorialVentas from "../page/HistorialVentas";
+
 
 function AdminView() {
-  return <div>Hola, admin</div>;
+  return <div>
+  <h1>Hola, admin</h1>
+  <div>
+  <BrowserRouter>
+  <nav>
+
+    <ul>
+      <li>
+      <Link to="/AddCliente">Añadir Cliente</Link>
+      </li>
+      <li>
+      <Link to="/AddProducto">Añadir Producto</Link>
+      </li>
+      <li>
+      <Link to="/AddProveedor">Añadir Proveedor</Link>
+      </li>
+    </ul>
+
+    <ul>
+    <li>
+      <Link to="/">Gestion Venta</Link>
+      </li>
+      <li>
+      <Link to="/GestionStock">Gestion Stock</Link>
+      </li>
+      <li>
+      <Link to="/historialVentas">Historial Ventas</Link>
+      </li>
+    </ul>
+
+  </nav>
+      <Routes>
+      <Route path="/" element={<GestionVenta />} />
+      <Route path="/AddCliente" element={<GestionCliente />} />
+      <Route path="/AddProducto" element={<GestionProducto />} />
+      <Route path="/AddProveedor" element={<GestionProveedor/>} />
+      <Route path="/GestionStock" element={<GestionStock />} />
+      <Route path="/historialVentas" element={<HistorialVentas />} />
+    </Routes>
+  </BrowserRouter>
+  </div>
+  </div>
 }
 
 export default AdminView;
